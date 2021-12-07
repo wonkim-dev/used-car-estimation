fd33f715b4bbdd595b349ab068f98011fcf4f9ba
wonkim-dev <wonkim.dev@gmail.com>
2021-12-07 21:39:07 +0100

    Implement find, update, remove methods in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,4 +16,31 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
+    // save() method executes hooks associated with the entity. insert() and update() do not.
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    // remove() method executes hooks associated with the entity. delete() does not.
+    return this.repo.remove(user);
+  }
 }
