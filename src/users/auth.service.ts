5287b1d7b7be030f47f03d77cc59cd315e3da1cf
wonkim-dev <wonkim.dev@gmail.com>
2021-12-16 15:29:04 +0100

    Implement signin mehtod in AuthService

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _ascrypt } from 'crypto';
 import { promisify } from 'util';
@@ -29,5 +33,19 @@ export class AuthService {
     return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('wrong password');
+    }
+    return user;
+  }
 }
