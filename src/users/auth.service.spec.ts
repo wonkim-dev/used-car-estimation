6c44b2c114f2dfbe3b0be134888c1e5321182d5b
wonkim-dev <wonkim.dev@gmail.com>
2021-12-31 14:04:49 +0100

    Add a unit test for signup method (error handling)

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
@@ -5,10 +6,11 @@ import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -35,5 +37,14 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if tuser signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+    await expect(
+      service.signup('email@email.com', 'password'),
+    ).rejects.toThrowError(BadRequestException);
   });
 });
