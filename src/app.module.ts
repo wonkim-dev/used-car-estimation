b5fa7fad14d5cc3e3e6d57bb61690d3bef479c32
wonkim-dev <wonkim.dev@gmail.com>
2022-02-23 13:07:22 +0100

    Implement typeorm migration for test, development and production environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,17 +16,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          entities: [User, Report],
-          synchronize: true,
-        };
-      },
-    }),
+    TypeOrmModule.forRoot(),
     UsersModule,
     ReportsModule,
   ],
